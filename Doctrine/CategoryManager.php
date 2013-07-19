3bcd4df30e2d17e442d022e713b464c51d2a89a4
Julien Kirsch <judas_le_philosophe@hotmail.com>
2013-07-19 18:32:39 -0500

    Added getChildrenCategories method

--- Doctrine/CategoryManager.php
+++ Doctrine/CategoryManager.php
@@ -96,6 +96,14 @@ class CategoryManager extends AbstractCategoryManager
     /**
      * {@inheritDoc}
      */
+    public function getChildrenCategories(CategoryInterface $category = null, $direct = false, $sortByField = null, $direction = 'ASC', $includeRootCategory = false)
+    {
+        return $this->repository->getChildren($category, $direct, $sortByField, $direction, $includeRootCategory);
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
     public function getPath(CategoryInterface $category)
     {
         return $this->repository->getPath($category);
